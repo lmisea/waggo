796d4b38c5da3185ad3e6ba596cee8d50400ee52
Luis Isea <isea.luis.miguel@gmail.com>
2026-06-17 19:30:30 -0400

    fix: improve name verification

--- src/types/booking.ts
+++ src/types/booking.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 export const BookingSchema = z.object({
   fullName: z
     .string()
-    .min(3, { message: "El nombre debe tener al menos 3 caracteres" }),
+    .min(3, { message: "El nombre debe tener al menos 3 caracteres" })
+    .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$/, {
+      message: "El nombre solo puede contener letras y espacios",
+    }),
   // Regex (0412, 0414, 0416, 0422, 0424, 0426 + 7 dígitos)
   phone: z.string().regex(/^04(12|14|16|22|24|26)\d{7}$/, {
     message: "Ingresa un número válido (ej. 04121234567)",
